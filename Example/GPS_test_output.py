2468cfc5d8f38247b19ca33c66253d5c39d83422
Quantz <quanhongdagm@gmail.com>
2022-08-07 10:41:51 +0800

    Examples for functions output

--- Example/GPS_test_output.py
+++ Example/GPS_test_output.py
@@ -1,12 +1,14 @@
-from GPS_Package import get_gps_data, draw_map
+from ..GPS_Package import get_gps_data, init_serial
 import time
 
 if __name__ == '__main__':
+    SER = init_serial("com7")
     while True:
-        cur_LATITUDE, cur_LONGITUDE, cur_LATITUDE_GM, cur_LONGITUDE_GM, NAVI_SPEED, NAVI_DIRECTION = get_gps_data(
-            "com3")
-        if cur_LATITUDE == '' and cur_LONGITUDE == '' and cur_LATITUDE_GM == '' and cur_LONGITUDE_GM == '':
+        try:
+            cur_LATITUDE, cur_LONGITUDE, cur_LATITUDE_GM, cur_LONGITUDE_GM, NAVI_SPEED, NAVI_DIRECTION = get_gps_data(
+                SER)
+            print(cur_LATITUDE, cur_LONGITUDE, cur_LATITUDE_GM, cur_LONGITUDE_GM, NAVI_SPEED, NAVI_DIRECTION)
+            # set time to refresh site
+            time.sleep(0.5)
+        except:
             continue
-        print(cur_LATITUDE, cur_LONGITUDE, cur_LATITUDE_GM, cur_LONGITUDE_GM, NAVI_SPEED, NAVI_DIRECTION)
-        # set time to refresh site
-        time.sleep(0.5)
